8f2ca670c43fbcae397161b242da3f2ac3a2cc6e
Moronya <moronyasociety@gmail.com>
2021-01-24 18:25:22 +0300

    added string functions

--- sorganizer/organizer/models.py
+++ sorganizer/organizer/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=60, unique=True)
     slug = models.SlugField(max_length=30, unique = True, help_text="A label for URL config. ")
+    def __str__(self):
+        return self.name
 
 class Startup(models.Model):
     name = models.CharField(max_length=60,db_index = True, help_text= 'A label for URL config. ') #when the db_index parameter is set to True, the values are automatically indexed in the database 
@@ -11,10 +13,14 @@ class Startup(models.Model):
     contact = models.EmailField()
     website = models.URLField(max_length = 255) 
     tags = models.ManyToManyField(Tag)
-
+    
 class NewsLink(models.Model):
     title = models.CharField(max_length=60)
     pub_date = models.DateField('date published')
     link = models.URLField()
     startup = models.ForeignKey(Startup)
+
+    def __str__(self):
+        return *{}:{}*.format(self.Startup, self.title)
+
 
