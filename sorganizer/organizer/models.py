3dccd41a8aad052911b8cb57c928777b3cdf62e4
Moronya <moronyasociety@gmail.com>
2021-01-24 17:23:52 +0300

    First Commit

---
+++ sorganizer/organizer/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    slug = models.SlugField(max_length=30, unique = True, help_text="A label for URL config. ")
+
+class Startup(models.Model):
+    name = models.CharField(max_length=60,db_index = True, help_text= 'A label for URL config. ') #when the db_index parameter is set to True, the values are automatically indexed in the database 
+    founded_date = models.DateField("Date founded")
+    contact = models.EmailField()
+    website = models.URLField(max_length = 255) 
+    tags = models.ManyToManyField(Tag)
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=60)
+    pub_date = models.DateField('date published')
+    link = models.URLField()
+    startup = models.ForeignKey(Startup)
+
